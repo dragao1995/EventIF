57369036fdc19f660834291402227f2faddd0401
dragao1995 <dragao1995@gmail.com>
2015-06-29 10:10:48 -0300

    alo2

--- EventIF/src/control/telaLogin.java
+++ EventIF/src/control/telaLogin.java
@@ -1,4 +1,5 @@
 package control;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -6,39 +7,33 @@ import view.TelaCadastro;
 import view.TelaLogin;
 
 public class telaLogin {
-TelaLogin tl = new TelaLogin();
-		
-		
-public TelaLogin getTl() {
-			return tl;
-		}
-public void setTl(TelaLogin tl) {
-			this.tl = tl;
-		}
+	TelaLogin tl = new TelaLogin();
 
+	public TelaLogin getTl() {
+		return tl;
+	}
 
-public void EventosTelaLogin(){
-	
-	tl.getBtnCadastro().addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent arg0) {
-			
-			TelaCadastro windowTC = new TelaCadastro();
-			windowTC.getFrame().setVisible(true);
-			tl.getFrameLogin().setVisible(false);
-					
-		}
-	});
-	
-	tl.getBtnLogin().addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent arg0) {
-			
-			
-					
-		}
-	});
-			
-	
-}
-	
+	public void setTl(TelaLogin tl) {
+		this.tl = tl;
+	}
 
+	public void EventosTelaLogin() {
+		tl.getFrameLogin().setVisible(true);
+		tl.getBtnCadastro().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				TelaCadastro windowTC = new TelaCadastro();
+				windowTC.getFrame().setVisible(true);
+				tl.getFrameLogin().setVisible(false);
+
+			}
+		});
+
+		tl.getBtnLogin().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+			}
+		});
+
+	}
 }
--- EventIF/src/main/main.java
+++ EventIF/src/main/main.java
@@ -5,18 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import control.telaLogin;
 
-
 public class main {
-	public static void main(String[] args) throws SQLException  {
+	public static void main(String[] args) throws SQLException {
 		telaLogin windowcontrol = new telaLogin();
-		Connection conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Projeto","postgres","123");
+		Connection conexao = DriverManager.getConnection(
+				"jdbc:postgresql://localhost:5432/Projeto", "postgres", "123");
 		conexao.close();
-		
-					//ativa tela login
-					windowcontrol.getTl().getFrameLogin().setVisible(true);
-					//chama o evento dos  botoes tela login
-					windowcontrol.EventosTelaLogin();
-					
+
+		// chama tela login
+		windowcontrol.EventosTelaLogin();
+
 	}
 
 }
