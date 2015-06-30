93fb5a25df5f34f40a029ef8b89493393df129c6
Maryana Marinho <Maryana Marinho@Maryana>
2015-06-30 10:28:43 -0300

    coloquei id em participante atividade

--- EventIF/src/br/edu/ifg/tads/mtp/eventif/model/Participante_Atividade.java
+++ EventIF/src/br/edu/ifg/tads/mtp/eventif/model/Participante_Atividade.java
@@ -2,10 +2,19 @@ package br.edu.ifg.tads.mtp.eventif.model;
 
 public class Participante_Atividade {
 	
+	private long idParticipante_ativ; // eu(mary) coloquei esse id, so que tem que mudar no banco
 	private Pessoa_Participante pessoa_part = new Pessoa_Participante();
 	private Atividade atividade;
 	private String checkin;
 	private String checkout;
+	
+	
+	public long getIdParticipante_ativ() {
+		return idParticipante_ativ;
+	}
+	public void setIdParticipante_ativ(long idParticipante_ativ) {
+		this.idParticipante_ativ = idParticipante_ativ;
+	}
 	public Pessoa_Participante getPessoa_part() {
 		return pessoa_part;
 	}
